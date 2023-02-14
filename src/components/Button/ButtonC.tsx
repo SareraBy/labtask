3f25b05602c1c25aba3f7504c6573b84d150be51
Sarera <Sarerabib@gmail.com>
2023-02-14 12:49:03 +0200

    components-button-readme

---
+++ src/components/Button/ButtonC.tsx
@@ -0,0 +1,15 @@
+import React, {FC} from 'react';
+import './button.css'
+export interface ButtonCProps{
+    children: React.ReactNode
+    onClick?: () => void
+}
+const ButtonClick:FC<ButtonCProps> = (props) => {
+    return (
+        <button onClick={props.onClick} className={"btn-click-m"} style={{ borderStyle:"1px solid dark", borderRadius:"0px", width:'100px', height:"50px"}}>
+            {props.children}
+        </button>
+    );
+};
+
+export default ButtonClick;
---
+++ src/pages/lab2/README.md
@@ -0,0 +1,15 @@
+
+# Лабраторна робота №2
+
+Задание:
+
+
+### Отладка программы:
+
+
+
+### Блок-схема:
+[Сайт блок схем]([../../linux/overview.md#managed-disks](https://lucid.app/lucidchart/01b8c902-9208-4152-9a84-7ae7631c6fb4/edit?beaconFlowId=578CC65C19D3C6AE&invitationId=inv_fecd7e32-e917-4ef1-a166-4283e75710a7&page=0_0#))
+
+
+
