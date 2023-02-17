09d7134b8ee29b096d5c4969de97e28327794048
Sarera <Sarerabib@gmail.com>
2023-02-17 23:56:38 +0200

    upgrade

--- src/components/Button/ButtonC.tsx
+++ src/components/Button/ButtonC.tsx
@@ -3,10 +3,12 @@ import './button.css'
 export interface ButtonCProps{
     children: React.ReactNode
     onClick?: () => void
+
 }
+
 const ButtonClick:FC<ButtonCProps> = (props) => {
     return (
-        <button onClick={props.onClick} className={"btn-click-m"} style={{ borderStyle:"1px solid dark", borderRadius:"0px", width:'100px', height:"50px"}}>
+        <button onClick={props.onClick} className={"btn-click-m m-1"} style={{ borderStyle:"1px solid dark", borderRadius:"0px", width:'100px', height:"50px"}}>
             {props.children}
         </button>
     );
